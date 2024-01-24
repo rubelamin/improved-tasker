35dc9703114a88d00b3b5afd9a6252d7f450e831
Rubel Amin <bagha.vai100@gmail.com>
2024-01-25 00:12:44 +0600

    Modal position fixed

--- src/components/modal/DeleteAllModal.jsx
+++ src/components/modal/DeleteAllModal.jsx
@@ -6,31 +6,32 @@ export default function DeleteAllModal({ setAllDeleteModal }) {
 	const { handleDeleteAll } = useContext(TaskContext);
 	return (
 		<>
-			<div className="bg-black bg-opacity-70 fixed  inset-0 z-0 min-h-screen"></div>
-			<div className="mx-auto absolute z-10 top-1/4 left-1/4 my-3 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
-				<h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-					Do you want to delete all tasks?
-				</h2>
-				<p className="text-red-300 animate-pulse">
-					If you perform this operation all the tasks will be deleted
-					and cannot be undone.
-				</p>
+			<div className="bg-black bg-opacity-70 fixed inset-0 z-50 h-full overflow-y-auto min-h-screen">
+				<div className="mx-auto absolute z-10 top-1/4 left-1/4 my-3 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
+					<h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
+						Do you want to delete all tasks?
+					</h2>
+					<p className="text-red-300 animate-pulse">
+						If you perform this operation all the tasks will be
+						deleted and cannot be undone.
+					</p>
 
-				<div className="mt-16 flex justify-between lg:mt-20">
-					<button
-						type="submit"
-						className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
-						onClick={() => setAllDeleteModal(false)}
-					>
-						Cancel
-					</button>
-					<button
-						type="submit"
-						className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-						onClick={handleDeleteAll}
-					>
-						Yes
-					</button>
+					<div className="mt-16 flex justify-between lg:mt-20">
+						<button
+							type="submit"
+							className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+							onClick={() => setAllDeleteModal(false)}
+						>
+							Cancel
+						</button>
+						<button
+							type="submit"
+							className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+							onClick={handleDeleteAll}
+						>
+							Yes
+						</button>
+					</div>
 				</div>
 			</div>
 		</>
--- src/components/modal/TaskModal.jsx
+++ src/components/modal/TaskModal.jsx
@@ -71,103 +71,106 @@ export default function TaskModal({ onClickModal }) {
 	}
 	return (
 		<>
-			<div className="bg-black bg-opacity-70 fixed inset-0 z-0 min-h-screen"></div>
-			<form className="mx-auto absolute z-10 top-1/4 left-1/4 my-3 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
-				<h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
-					Add New Task
-				</h2>
+			<div className="bg-black bg-opacity-70 fixed inset-0 z-50 h-full overflow-y-auto min-h-screen">
+				<form className="mx-auto absolute z-10 top-1/4 left-1/4 my-3 w-full max-w-[740px] rounded-xl border border-[#FEFBFB]/[36%] bg-[#191D26] p-9 max-md:px-4 lg:my-20 lg:p-11">
+					<h2 className="mb-9 text-center text-2xl font-bold text-white lg:mb-11 lg:text-[28px]">
+						Add New Task
+					</h2>
 
-				<div className="space-y-9 text-white lg:space-y-10">
-					<div className="space-y-2 lg:space-y-3">
-						<label htmlFor="title">Title</label>
-						<input
-							className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
-							type="text"
-							name="title"
-							id="title"
-							value={taskElm.title}
-							onChange={handleTaskChange}
-							required
-						/>
-						{errors.title && (
-							<span className="text-red-500">{errors.title}</span>
-						)}
-					</div>
-					<div className="space-y-2 lg:space-y-3">
-						<label htmlFor="description">Description</label>
-						<textarea
-							className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
-							type="text"
-							name="description"
-							id="description"
-							value={taskElm.description}
-							onChange={handleTaskChange}
-							required
-						></textarea>
-						{errors.description && (
-							<span className="text-red-500">
-								{errors.description}
-							</span>
-						)}
-					</div>
-					<div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+					<div className="space-y-9 text-white lg:space-y-10">
 						<div className="space-y-2 lg:space-y-3">
-							<label htmlFor="tags">Tags</label>
+							<label htmlFor="title">Title</label>
 							<input
 								className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
 								type="text"
-								name="tags"
-								id="tags"
-								value={taskElm.tags}
+								name="title"
+								id="title"
+								value={taskElm.title}
 								onChange={handleTaskChange}
 								required
 							/>
-							{errors.tags && (
+							{errors.title && (
 								<span className="text-red-500">
-									{errors.tags}
+									{errors.title}
 								</span>
 							)}
 						</div>
 						<div className="space-y-2 lg:space-y-3">
-							<label htmlFor="priority">Priority</label>
-							<select
-								className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
-								name="priority"
-								id="priority"
-								value={taskElm.priority}
+							<label htmlFor="description">Description</label>
+							<textarea
+								className="block min-h-[120px] w-full rounded-md bg-[#2D323F] px-3 py-2.5 lg:min-h-[180px]"
+								type="text"
+								name="description"
+								id="description"
+								value={taskElm.description}
 								onChange={handleTaskChange}
 								required
-							>
-								<option value="">Select Priority</option>
-								<option value="Low">Low</option>
-								<option value="Medium">Medium</option>
-								<option value="High">High</option>
-							</select>
-							{errors.priority && (
+							></textarea>
+							{errors.description && (
 								<span className="text-red-500">
-									{errors.priority}
+									{errors.description}
 								</span>
 							)}
 						</div>
+						<div className="grid-cols-2 gap-x-4 max-md:space-y-9 md:grid lg:gap-x-10 xl:gap-x-20">
+							<div className="space-y-2 lg:space-y-3">
+								<label htmlFor="tags">Tags</label>
+								<input
+									className="block w-full rounded-md bg-[#2D323F] px-3 py-2.5"
+									type="text"
+									name="tags"
+									id="tags"
+									value={taskElm.tags}
+									onChange={handleTaskChange}
+									required
+								/>
+								{errors.tags && (
+									<span className="text-red-500">
+										{errors.tags}
+									</span>
+								)}
+							</div>
+							<div className="space-y-2 lg:space-y-3">
+								<label htmlFor="priority">Priority</label>
+								<select
+									className="block w-full cursor-pointer rounded-md bg-[#2D323F] px-3 py-2.5"
+									name="priority"
+									id="priority"
+									value={taskElm.priority}
+									onChange={handleTaskChange}
+									required
+								>
+									<option value="">Select Priority</option>
+									<option value="Low">Low</option>
+									<option value="Medium">Medium</option>
+									<option value="High">High</option>
+								</select>
+								{errors.priority && (
+									<span className="text-red-500">
+										{errors.priority}
+									</span>
+								)}
+							</div>
+						</div>
 					</div>
-				</div>
-				<div className="mt-16 flex justify-between lg:mt-20">
-					<button
-						type="submit"
-						className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
-						onClick={onClickModal}
-					>
-						Cancel
-					</button>
-					<button
-						type="submit"
-						className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
-						onClick={(e) => onSaveTask(e)}
-					>
-						Create new Task
-					</button>
-				</div>
-			</form>
+					<div className="mt-16 flex justify-between lg:mt-20">
+						<button
+							type="submit"
+							className="rounded bg-red-600 px-4 py-2 text-white transition-all hover:opacity-80"
+							onClick={onClickModal}
+						>
+							Cancel
+						</button>
+						<button
+							type="submit"
+							className="rounded bg-blue-600 px-4 py-2 text-white transition-all hover:opacity-80"
+							onClick={(e) => onSaveTask(e)}
+						>
+							Create new Task
+						</button>
+					</div>
+				</form>
+			</div>
 		</>
 	);
 }
